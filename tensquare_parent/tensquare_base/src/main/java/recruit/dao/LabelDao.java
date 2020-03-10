3d89255038ae9a857165c21fd53a4549eb5c9498
wangbo <w1114034411@outlook.com>
2020-03-10 17:46:02 +0800

    通用模块，城市，标签

---
+++ tensquare_parent/tensquare_base/src/main/java/recruit/dao/LabelDao.java
@@ -0,0 +1,9 @@
+package recruit.dao;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import recruit.pojo.Label;
+
+public interface LabelDao extends JpaRepository<Label, String>, JpaSpecificationExecutor<Label> {
+}
